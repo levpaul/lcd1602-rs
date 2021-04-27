c74bea31fb63f7693b09793f2afe08ba8b2264d5
Levi Lovelock <lovelock.levi@gmail.com>
2021-04-27 00:24:51 -0700

    Moved from systick to gpt

--- src/liquid_crystal.rs
+++ src/liquid_crystal.rs
@@ -1,4 +1,5 @@
 use embedded_hal::digital::v2::OutputPin;
+use imxrt_hal::gpt::GPT;
 use teensy4_bsp::SysTick;
 
 pub struct LCD<'a, A, B, C, D, E, F> {
@@ -8,7 +9,7 @@ pub struct LCD<'a, A, B, C, D, E, F> {
     pub d5: &'a mut D,
     pub d6: &'a mut E,
     pub d7: &'a mut F,
-    pub st: &'a mut SysTick,
+    pub gpt: &'a mut GPT,
 }
 
 impl<'a, A, B, C, D, E, F> LCD<'a, A, B, C, D, E, F>
@@ -21,19 +22,23 @@ where
     F: OutputPin,
 {
     pub fn init(&mut self) {
-        self.st.delay(50);
-        self.command(0x00);
-        self.st.delay(5);
+        self.delay(50000);
+        self.command(0x00); //4 bit shuffle
+        self.delay(150);
         self.write4(0x03);
+        self.delay(150);
+        self.write4(0x03);
+        self.delay(150);
         self.write4(0x02);
 
         self.command(0x0C); // Display mode
         self.command(0x01); // Clear
+        self.delay(2900); // Delay per homing
         self.command(0x06); // Entrymode
     }
 
     pub fn command(&mut self, cmd: u8) {
-        self.st.delay(3); // per char delay
+        self.delay(320); // per char delay
         self.rs.set_low();
         // self.write4(cmd & 0x0F); // 4bit writes send end pulses
         // self.write4(cmd & 0xF0);
@@ -42,8 +47,7 @@ where
     }
 
     pub fn write_char(&mut self, ch: u8) {
-        log::info!("Writing char");
-        self.st.delay(3); // per char delay
+        self.delay(320); // per char delay
         self.rs.set_high();
         self.write4(ch & 0x0F); // 4bit writes send end pulses
         self.write4((ch & 0xF0) >> 4);
@@ -72,10 +76,26 @@ where
             self.d7.set_low();
         }
         self.en.set_high();
+        self.delay(1);
         self.en.set_low();
     }
 
-    pub fn delay(&mut self, interval_ms: u32) {
-        self.st.delay(interval_ms);
+    pub fn delay(&mut self, interval_us: u64) {
+        self.gpt.set_output_compare_duration(
+            imxrt_hal::gpt::OutputCompareRegister::Three,
+            core::time::Duration::from_micros(interval_us),
+        );
+        loop {
+            let mut res = self
+                .gpt
+                .output_compare_status(imxrt_hal::gpt::OutputCompareRegister::Three);
+            if res.is_set() {
+                res.clear();
+                return;
+            }
+        }
     }
+    // pub fn delay(&mut self, interval_ms: u32) {
+    //     self.st.delay(interval_ms);
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@
 #![no_main]
 #![allow(unused_must_use)]
 
+use core::convert::Infallible;
+use nb::block;
 use teensy4_bsp as bsp;
 use teensy4_bsp::hal::gpio;
+use teensy4_bsp::SysTick;
 use teensy4_panic as _;
 
 mod liquid_crystal;
@@ -16,8 +19,8 @@ const LED_PERIOD_MS: u32 = 1_000;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let p = bsp::Peripherals::take().unwrap();
-    let mut systick = bsp::SysTick::new(cortex_m::Peripherals::take().unwrap().SYST);
+    let mut p = bsp::Peripherals::take().unwrap();
+    let mut systick = SysTick::new(cortex_m::Peripherals::take().unwrap().SYST); // Now unused in favor of GPT for finer granularity
     let pins = bsp::t40::into_pins(p.iomuxc);
     let mut led = bsp::configure_led(pins.p13);
 
@@ -30,6 +33,24 @@ fn main() -> ! {
     let mut d5 = gpio::GPIO::new(pins.p4).output();
     let mut d6 = gpio::GPIO::new(pins.p3).output();
     let mut d7 = gpio::GPIO::new(pins.p2).output();
+    let mut dd = gpio::GPIO::new(pins.p6).output();
+
+    // GPT setup
+    let (_, ipg_hz) =
+        p.ccm
+            .pll1
+            .set_arm_clock(imxrt_hal::ccm::PLL1::ARM_HZ, &mut p.ccm.handle, &mut p.dcdc);
+
+    let mut cfg = p.ccm.perclk.configure(
+        &mut p.ccm.handle,
+        imxrt_hal::ccm::perclk::PODF::DIVIDE_3,
+        imxrt_hal::ccm::perclk::CLKSEL::IPG(ipg_hz),
+    );
+    let mut gpt1 = p.gpt1.clock(&mut cfg);
+    gpt1.set_output_interrupt_on_compare(imxrt_hal::gpt::OutputCompareRegister::Three, false);
+    gpt1.set_mode(imxrt_hal::gpt::Mode::FreeRunning);
+    gpt1.set_reset_on_enable(true);
+    gpt1.set_enable(true);
 
     let mut lcd = liquid_crystal::LCD {
         en: &mut en,
@@ -38,16 +59,19 @@ fn main() -> ! {
         d5: &mut d5,
         d6: &mut d6,
         d7: &mut d7,
-        st: &mut systick,
+        gpt: &mut gpt1,
     };
+
+    lcd.delay(2000);
+    log::info!("Init");
 
     lcd.init();
     let mut i = 20;
     loop {
         i = i + 1 % 255;
         led.toggle();
-        lcd.delay(LED_PERIOD_MS);
         lcd.write_char(i);
-        log::info!("Hello world");
+        dd.toggle();
+        lcd.delay(100000);
     }
 }
